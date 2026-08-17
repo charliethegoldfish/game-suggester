704b4a3bd5c81e486780a9c22e8677323ae2c746
Charlie Cassidy <charliefrancis.cassidy@gmail.com>
2026-08-17 18:28:49 +1000

    stubbing out some extract functions for the converter

---
+++ md_to_csv/extract_functions.py
@@ -0,0 +1,23 @@
+
+def extract_name_from_title(title: str) -> str:
+	pass
+
+# functions that process and return properties found in the md yaml portion
+def extract_genres_from_yaml(yaml: str) -> list[str]:
+	pass
+
+def extract_platforms_from_yaml(yaml: str) -> list[str]:
+	pass
+
+def extract_status_from_yaml(yaml: str) -> str:
+	pass
+
+def extract_related_games_from_yaml(yaml: str) -> str:
+	pass
+
+# generic function to grab the raw str for a given property
+def extract_property_raw_from_yaml(yaml: str, property_name: str) -> str:
+	pass
+
+def extract_yaml_from_md(markdown: str) -> str:
+	pass
