4dd788a28094038f59c856a9118e54a82bb3224a
Charlie Cassidy <charliefrancis.cassidy@gmail.com>
2026-08-23 15:05:50 +1000

    compiling dict of games with name as key and lists of available options to filter with

--- game_suggester/load_game_functions.py
+++ game_suggester/load_game_functions.py
@@ -1,7 +1,7 @@
 from core.gamenode import GameNode
 from core.csvnode import CsvNode
 from core.csv_functions import csv_to_nodes
-from core.core_config import LIST_FIELDS
+from core.core_config import *
 from core.format_functions import format_str_to_list
 
 def process_csv_node(node: CsvNode, verbose: bool) -> None:
@@ -24,6 +24,29 @@ def load_game_nodes(file_path: str, verbose: bool) -> list[GameNode]:
 	for node in csv_nodes:
 		process_csv_node(node, verbose)
 
-	
-
 	return csv_nodes
+
+def add_list_item(items_to_add: list[str], items: list[str]) -> None:
+	for item in items_to_add:
+		if item in items or item == '':
+			continue
+		items.append(item)
+
+def add_item(item: str, items: list[str]) -> None:
+	if item in items:
+		return
+	items.append(item)
+
+def construct_game_library_data(nodes: list[GameNode], platforms: list[str], genres: list[str], status_options: list[str], tags: list[str], verbose: bool) -> dict[str, GameNode]:
+	library = {}
+	for node in nodes:
+		# Create an entry in the game library dict
+		library[node.name] = node
+
+		# Add to our list of things if it's not already in there
+		add_list_item(node.details[GENRES_KEY], genres)
+		add_list_item(node.details[PLATFORMS_KEY], platforms)
+		add_item(node.details[STATUS_KEY], status_options)
+		add_list_item(node.details[TAGS_KEY], tags)
+
+	return library
--- game_suggester/suggester.py
+++ game_suggester/suggester.py
@@ -1,7 +1,7 @@
 import os
 from core.gamenode import GameNode
 from core.csv_functions import init_csv_dialect
-from game_suggester.load_game_functions import load_game_nodes
+from game_suggester.load_game_functions import load_game_nodes, construct_game_library_data
 
 def suggester(csv_path: str, verbose: bool) -> None:
 	
@@ -12,6 +12,19 @@ def suggester(csv_path: str, verbose: bool) -> None:
 
 		abs_csv_path = os.path.abspath(csv_path)
 		game_nodes = load_game_nodes(abs_csv_path, verbose)
+
+		genres = []
+		platforms = []
+		status_options = []
+		tags = []
+
+		game_library = construct_game_library_data(game_nodes, platforms, genres, status_options, tags, verbose)
+
+		if verbose:
+			print(f'Available platforms: {platforms}')
+			print(f'Available genres: {genres}')
+			print(f'Available status options: {status_options}')
+			print(f'Available tags: {tags}')
 
 	except Exception as e:
 		print(f'Error suggesting game: {e}')
