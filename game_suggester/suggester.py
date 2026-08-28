cb35d339c608bb356bfef8a3f366fdb6f339de00
Charlie Cassidy <charliefrancis.cassidy@gmail.com>
2026-08-28 17:58:14 +1000

    adding platform and status combo boxes to suggester widget

--- game_suggester/suggester.py
+++ game_suggester/suggester.py
@@ -30,7 +30,7 @@ def suggester(csv_path: str, verbose: bool) -> None:
 			print(f'Available tags: {tags}')
 
 		app = QApplication(sys.argv)
-		ex = SuggesterApp(genres, platforms, status_options, tags)
+		ex = SuggesterApp(game_nodes, genres, platforms, status_options, tags)
 		sys.exit(app.exec())
 
 	except Exception as e:
--- game_suggester/suggester_app.py
+++ game_suggester/suggester_app.py
@@ -1,13 +1,14 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QComboBox, QLabel, QHBoxLayout, QVBoxLayout
 from PyQt6.QtGui import QIcon
+from core.gamenode import GameNode
 
 def init_widget_details(widget: QWidget, name: str, tooltip: str):
 	widget.setObjectName(name)
 	widget.setToolTip(tooltip)
 
 class SuggesterApp(QWidget):
-	def __init__(self, genres: list[str], platforms: list[str], status_options: list[str], tags: list[str]):
+	def __init__(self, game_nodes: list[GameNode],genres: list[str], platforms: list[str], status_options: list[str], tags: list[str]):
 		super().__init__()
 		self.title = 'Game Suggester'
 		self.left = 200
@@ -15,6 +16,7 @@ class SuggesterApp(QWidget):
 		self.width = 400
 		self.height = 200
 
+		self._game_nodes = game_nodes
 		self._genres = genres
 		self._platforms = platforms
 		self._status_options = status_options
@@ -27,12 +29,34 @@ class SuggesterApp(QWidget):
 		self.setGeometry(self.left, self.top, self.width, self.height)
 
 		# Setup platform combo box
+		self._platform_combobox = QComboBox()
+		init_widget_details(self._platform_combobox, "platformComboBox", "Platform you want to play on")
+		self._platform_combobox.addItems(self._platforms)
+
+		platform_label = QLabel("Platform:")
+		platform_label.setBuddy(self._platform_combobox)
 
 		# Setup status combo box
+		self._status_combobox = QComboBox()
+		init_widget_details(self._status_combobox, "statusComboBox", "Status of game you want to play")
+		self._status_combobox.addItems(self._status_options)
+
+		status_label = QLabel("Status:")
+		status_label.setBuddy(self._status_combobox)
+
+		top_layout = QHBoxLayout()
+		top_layout.addWidget(platform_label)
+		top_layout.addWidget(self._platform_combobox)
+		top_layout.addStretch(1)
+		top_layout.addWidget(status_label)
+		top_layout.addWidget(self._status_combobox)
 
 		# Setup grid of genre checkboxes
 
 		# Setup grid of tag checkboxes
+
+		main_layout = QVBoxLayout(self)
+		main_layout.addLayout(top_layout)
 
 		self.show()
 
